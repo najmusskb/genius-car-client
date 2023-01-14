88314fbf4e7e8ebdccd16410bde526f99af26c24
najmusskb <najmussakib173@gmail.com>
2023-01-14 12:51:44 +0600

    services part

---
+++ src/Pages/Home/Home/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className="text-2xl font-blod text-orange-600">Services</p>
+                <h2 className='text-5xl font-semiblod'>Our Service Area</h2>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
