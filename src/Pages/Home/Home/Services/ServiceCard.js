3055ac9cbbd4d8eecf1434d9c888a179160abc0d
najmusskb <najmussakib173@gmail.com>
2023-01-15 12:18:09 +0600

    make environment config

--- src/Pages/Home/Home/Services/ServiceCard.js
+++ src/Pages/Home/Home/Services/ServiceCard.js
@@ -4,7 +4,7 @@ import Services from './Services';
 const ServiceCard = ({ service }) => {
     const { img, price, title } = service;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 shadow-xl border rounded mb-5">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
