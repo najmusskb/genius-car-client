5403ecc9c51a85697ab73275b6a5b43d16da2727
najmusskb <najmussakib173@gmail.com>
2023-01-15 11:30:16 +0600

    data loader

---
+++ src/Pages/Home/Home/Services/ServiceCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Services from './Services';
+
+const ServiceCard = ({ service }) => {
+    const { img, price, title } = service;
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p className='text-2xl  text-orange-600 font-semibold'>${price}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
--- src/Pages/Home/Home/Services/Services.js
+++ src/Pages/Home/Home/Services/Services.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+
+    }, [])
     return (
         <div>
             <div className='text-center'>
@@ -8,8 +17,14 @@ const Services = () => {
                 <h2 className='text-5xl font-semiblod'>Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
             </div>
+
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
-
 export default Services;
